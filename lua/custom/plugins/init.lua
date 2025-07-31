c2712b336a1c77c6226aa4340ecc22b940980153
KSayanta <181443436+KSayanta@users.noreply.github.com>
2025-07-31 11:18:51 +0530

    add color picker

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -275,4 +275,21 @@ return {
       vim.keymap.set('n', '<leader>tt', '<Cmd>TransparentToggle<CR>', { desc = '[T]oggle [T]ransparent UI' })
     end,
   },
+
+  { -- NOTE: oklch-color-picker
+    'eero-lehtinen/oklch-color-picker.nvim',
+    event = 'VeryLazy',
+    version = '*',
+    keys = {
+      {
+        '<leader>c',
+        function()
+          require('oklch-color-picker').pick_under_cursor()
+        end,
+        desc = '[C]olor pick under cursor',
+      },
+    },
+    ---@type oklch.Opts
+    opts = {},
+  },
 }
